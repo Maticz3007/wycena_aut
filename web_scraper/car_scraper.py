998aefa5c59b6b2d996408727d289e14a7a24a7a
czyzewm1 <01187529@pw.edu.pl>
2025-04-24 16:40:15 +0200

    Poprawka działania scrapera, logging, ok. 1200 aut

--- web_scraper/car_scraper.py
+++ web_scraper/car_scraper.py
@@ -1,3 +1,5 @@
+import logging
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -8,6 +10,17 @@ from selenium.webdriver.chrome.service import Service
 from datetime import datetime
 import csv
 import time
+
+CSV_TARGET = "cars.csv"
+CSS_SELECTORS = {
+    "ad_parameters": 'div[data-testid="ad-parameters-container"]',
+    "description": 'div[data-testid="ad_description"]',
+    "price": 'div[data-testid="ad-price-container"]',
+    "location": 'div[class="css-13l8eec"]',
+    "title": 'div[data-testid="ad_title"]',
+    "ad_id": 'span[class="css-w85dhy"]',
+    "breadcrumbs": 'ol[data-testid="breadcrumbs"] li',
+}
 
 
 def get_element_text(locator, retries=5):
@@ -32,24 +45,27 @@ def get_elements_text(locator, retries=5):
 
 def scrape_url(url):
     driver.get(url)
-    specs = get_element_text((By.CSS_SELECTOR, 'div[data-testid="ad-parameters-container"]'))
-    description = get_element_text((By.CSS_SELECTOR, 'div[data-testid="ad_description"]'))
-    price = get_element_text((By.CSS_SELECTOR, 'div[data-testid="ad-price-container"]'))
-    location = get_element_text((By.CSS_SELECTOR, 'div[class="css-13l8eec"]'))
-    # date = get_element_text((By.CSS_SELECTOR, 'span[data-cy="ad-posted-at"]')) #czasami jest data, czasami jest "dzisiaj o ..."
-    title = get_element_text((By.CSS_SELECTOR, 'div[data-testid="ad_title"]'))
-    ad_id = get_element_text((By.CSS_SELECTOR, 'span[class="css-w85dhy"]'))
-    breadcrumbs = get_elements_text((By.CSS_SELECTOR, 'ol[data-testid="breadcrumbs"] li'))
-    if len(breadcrumbs) >= 4:
-        brand = breadcrumbs[3]
-    else:
-        brand = "Pozostałe osobowe"
-    add_to_csv(specs, price, location, title, ad_id, brand, url, description)
+    try:
+        specs = get_element_text((By.CSS_SELECTOR, CSS_SELECTORS["ad_parameters"]))
+        description = get_element_text((By.CSS_SELECTOR, CSS_SELECTORS["description"]))
+        price = get_element_text((By.CSS_SELECTOR, CSS_SELECTORS["price"]))
+        location = get_element_text((By.CSS_SELECTOR, CSS_SELECTORS["location"]))
+        title = get_element_text((By.CSS_SELECTOR, CSS_SELECTORS["title"]))
+        ad_id = get_element_text((By.CSS_SELECTOR, CSS_SELECTORS["ad_id"]))
+        breadcrumbs = get_elements_text((By.CSS_SELECTOR, CSS_SELECTORS["breadcrumbs"]))
+        # date = get_element_text((By.CSS_SELECTOR, 'span[data-cy="ad-posted-at"]')) #czasami jest data, czasami jest "dzisiaj o ...", nie są to dane potrzebme
+        if len(breadcrumbs) >= 4:
+            brand = breadcrumbs[3]
+        else:
+            brand = "Pozostałe osobowe"
+        add_to_csv(specs, price, location, title, ad_id, brand, url, description)
+    except Exception as e:
+        logging.error("Failed to load advertisement %s, skipping", url)
+        logging.error(e)
     return
 
 
 def add_to_csv(specs, price, location, title, ad_id, brand, url, description):
-    csv_target = "cars.csv"
     dictionary = {column: "" for column in [
         "Numer VIN", "Model", "Rok produkcji", "Paliwo", "Typ nadwozia", "Przebieg",
         "Kolor", "Poj. silnika", "Stan techniczny", "Skrzynia biegów", "Kraj pochodzenia",
@@ -57,19 +73,11 @@ def add_to_csv(specs, price, location, title, ad_id, brand, url, description):
         "Tytuł", "Rodzaj ogłoszenia", "Znalezione o", "Link", "ID", "Producent"
     ]}
     id = ad_id.replace("ID:", "").strip()
-    print(datetime.now(), "Processing ID", id)
-    existing_ids = set()
+    logging.info("Processing ID %s", id)
 
-    try:
-        with open(csv_target, mode="r", newline="", encoding="utf-8") as file:
-            reader = csv.DictReader(file)
-            for row in reader:
-                existing_ids.add(row["ID"])
-    except FileNotFoundError:
-        pass
 
     if id in existing_ids:
-        print("ID" , id, "already exists, skipping.")
+        logging.info("ID %s already exists, skipping.", id)
         return
     specs_copy = specs.splitlines().copy()
     for item in specs_copy[1:]:
@@ -90,45 +98,80 @@ def add_to_csv(specs, price, location, title, ad_id, brand, url, description):
     dictionary["Znalezione o"] = datetime.now()
     dictionary["Link"] = url
     dictionary["Opis"] = description.replace("OPIS\n", "").strip()
-
-    with open(csv_target, mode="a", newline="", encoding="utf-8") as file:
+    logging.info("ID %s not found, adding to file.", id)
+    with open(CSV_TARGET, mode="a", newline="", encoding="utf-8") as file:
         writer = csv.DictWriter(file, fieldnames=dictionary.keys())
 
         if file.tell() == 0:
             writer.writeheader()
 
         writer.writerow(dictionary)
-    print("ID", id, "not found, adding to file with data: ", dictionary)
+    existing_ids.add(id)
     return
 
 
 def get_ads():
-    driver.get("https://www.olx.pl/motoryzacja/samochody/")
-    ads = driver.find_elements(By.CSS_SELECTOR, 'div[data-testid="l-card"]')
+    current_url = "https://www.olx.pl/motoryzacja/samochody/"
     links = []
-    for ad in ads:
+    for i in range(1, 26):
+        driver.get(current_url)
+        logging.info("Scraper obtained page %s.", i)
+        ads = driver.find_elements(By.CSS_SELECTOR, 'div[data-testid="l-card"]')
+        for ad in ads:
+            try:
+                link = ad.find_element(By.CSS_SELECTOR, 'a').get_attribute('href')
+                if (link.find("otomoto") == -1):
+                    links.append(link)
+            except Exception as e:
+                logging.critical(f"Error: {e}")
+        logging.info("Scraper found %s URLs to scrape from.", len(ads))
         try:
-            link = ad.find_element(By.CSS_SELECTOR, 'a').get_attribute('href')
-            if (link.find("otomoto") == -1):
-                links.append(link)
+            current_url = driver.find_element(By.CSS_SELECTOR, 'a[data-testid="pagination-forward"]').get_attribute('href')
         except Exception as e:
-            print(f"Error: {e}")
-    print("Got ", len(links), " URLs to scrape from.")
+            if i==25:
+                logging.info("Scraper obtained all available pages.")
+            else:
+                logging.error("Scraper failed to find next page, current page = %s", i)
+    logging.info("Scraper found a total of %s URLs to scrape from.", len(links))
+
     return links
 
-options = webdriver.ChromeOptions()
-options.add_argument("--headless")
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-wait = WebDriverWait(driver, 15)
+def init_driver():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+
+# "main"
+
+
+driver = init_driver()
+wait = WebDriverWait(driver, 30)
+logging.basicConfig(level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S', encoding="utf-8",
+                    handlers=[logging.StreamHandler(), logging.FileHandler("scrape.log")],
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+existing_ids = set()
+try:
+    with open(CSV_TARGET, mode="r", newline="", encoding="utf-8") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            existing_ids.add(row["ID"])
+except FileNotFoundError:
+    pass
+
+
 
 try:
+    logging.warning("Scraper active.")
     while True:
         for link in get_ads():
             scrape_url(link)
-        print(datetime.now(), "Scrape complete; waiting for 10 minutes before trying again...")
-        time.sleep(10 * 60)
+        logging.warning("Scrape complete; waiting for 6 hours before trying again...")
+        logging.info("Consider running the scraper manually sometime later.")
+        time.sleep(6 * 60 * 60)
 except KeyboardInterrupt:
-    print("Scraper stopped by user")
+    logging.critical("Scraper stopped by user")
     driver.quit()
     exit(1)
 
