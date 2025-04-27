0cb2d736199968ec0dbea2d739760bc4caa416b4
czyzewm1 <01187529@pw.edu.pl>
2025-04-27 23:18:22 +0200

    Czyszczenie zbioru danych

--- web_scraper/car_scraper.py
+++ web_scraper/car_scraper.py
@@ -21,7 +21,12 @@ CSS_SELECTORS = {
     "ad_id": 'span[class="css-w85dhy"]',
     "breadcrumbs": 'ol[data-testid="breadcrumbs"] li',
 }
-
+BASE_DICTIONARY = {column: "" for column in [
+        "Numer VIN", "Model", "Rok produkcji", "Paliwo", "Typ nadwozia", "Przebieg",
+        "Kolor", "Poj. silnika", "Stan techniczny", "Skrzynia biegów", "Kraj pochodzenia",
+        "Moc silnika", "Napęd", "Kierownica", "Cena", "Lokalizacja", "Województwo",
+        "Tytuł", "Rodzaj ogłoszenia", "Znalezione o", "Link", "ID", "Producent"
+    ]}
 
 def get_element_text(locator):
     try:
@@ -65,13 +70,7 @@ def scrape_url(url):
 
 
 def add_to_csv(specs, price, location, title, ad_id, brand, url, description):
-    dictionary = {column: "" for column in [
-        "Numer VIN", "Model", "Rok produkcji", "Paliwo", "Typ nadwozia", "Przebieg",
-        "Kolor", "Poj. silnika", "Stan techniczny", "Skrzynia biegów", "Kraj pochodzenia",
-        "Moc silnika", "Napęd", "Kierownica", "Cena", "Lokalizacja", "Województwo",
-        "Tytuł", "Rodzaj ogłoszenia", "Znalezione o", "Link", "ID", "Producent"
-    ]}
-
+    dictionary = BASE_DICTIONARY.copy()
     specs_copy = specs.splitlines().copy()
     for item in specs_copy[1:]:
         key, value = item.split(": ")
